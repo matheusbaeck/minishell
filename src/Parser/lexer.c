a085bb654b09fb358e6ac4d512f7661294ce72ee
matheusbaeck <matheusbaeck@gmail.com>
2024-01-31 13:57:16 +0100

    complete change lexer\n bug fix: now parsing of pipes is more protected (ls| or ls || ls)

--- src/Parser/lexer.c
+++ src/Parser/lexer.c
@@ -6,89 +6,54 @@
 /*   By: math <math@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 15:57:42 by smagniny          #+#    #+#             */
-/*   Updated: 2024/01/31 13:39:25 by math             ###   ########.fr       */
+/*   Updated: 2024/01/31 13:55:36 by math             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header.h"
 
-static int	count_lines(char **str)
+static int	get_pipes(t_var *var, t_list **list)
 {
-	int	i;
-
-	if (!str || !*str)
-		return (0);
-	i = 0;
-	while (str[i])
-		++i;
-	return (i);	
-}
-
-
-// static int	ft_lstsize_node(t_node *lst)
-// {
-// 	size_t	i;
-// 	t_node	*ptr;
-
-// 	i = 0;
-// 	ptr = lst;
-// 	while (ptr)
-// 	{
-// 		ptr = ptr->next;
-// 		i++;
-// 	}
-// 	return (i);
-// }
-
-static int	get_pipes(t_var *var)
-{
-	t_list	*list;
 	t_list	*temp;
 
-	list = ft_lstnew(ft_strdup(var->inputline));
-	if (pipe_split(&list, is_cut_point) == -1)
-		return (ft_lstclear(&list, free), free(var->inputline), -1);
-	temp = list;
+	*list = ft_lstnew(var->inputline);
+	if (pipe_split(list, is_cut_point) == -1)
+		return (ft_lstclear(list, free), free(var->inputline), -1);
+	temp = *list;
 	while (temp)
 	{
-		printf("%s\n", (char *)temp->content);
 		temp = temp->next;
 	}
-	ft_lstclear(&list, free);
 	return (0);
 }
 
 int	lexer(t_var *var)
 {
-	int			i;
 	int			start;
-	char		**splitted_inputline;
 	t_node		*first_node;
+	t_list		*list;
+	t_list		*current;
 
-	if (get_pipes(var) == -1)
+	if (get_pipes(var, &list) == -1)
 		return(var->exit_status = SYNTAX_ERROR, -1);
 	first_node = NULL;
-	splitted_inputline = ft_split(var->inputline, '|');
-	free(var->inputline);
-	var->nb_node = count_lines(splitted_inputline);
-	i = -1;
-	while (++i < var->nb_node)
+	current = list;
+	while (current)
 	{
-		start = 0;
-		var->inputline = splitted_inputline[i];
+		var->inputline = ((char *)current->content);
 		var->len_inputline = ft_strlen(var->inputline);
 		var->tokens = ft_lstnew_node();
+		start = 0;
 		while (start < var->len_inputline)
 			start = gnt_startpoint(var, start);
-		if (i == 0)
+		if (list == current)
 			first_node = var->tokens;
-		free(splitted_inputline[i]);
-		if (i != 0)
+		else
 			ft_lstadd_back_node(&first_node, var->tokens);
+		current = current->next;
 	}
-	if (splitted_inputline)
-		free(splitted_inputline);
 	var->tokens = first_node;
+	ft_lstclear(&list, free);
 	return (0);
 }
 //gnt_startpoint returns the index of the next character
