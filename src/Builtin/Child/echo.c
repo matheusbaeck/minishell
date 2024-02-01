a70381158ab304df37288c8edd42ef0cc66ad602
matheusbaeck <matheusbaeck@gmail.com>
2024-02-01 19:47:47 +0100

    bug fix: now all flags and parametres are set on tokens->params together, hardcoded parsin of echo moved to echo.c file. Not best implementation but works, some leaks

--- src/Builtin/Child/echo.c
+++ src/Builtin/Child/echo.c
@@ -6,11 +6,33 @@
 /*   By: math <math@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 14:07:23 by mohafnh           #+#    #+#             */
-/*   Updated: 2024/01/25 02:24:44 by math             ###   ########.fr       */
+/*   Updated: 2024/02/01 19:44:29 by math             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../header.h"
+
+static	int	detect_flag(t_node *node, char *token_string)
+{
+	int			i;
+
+	i = 1;
+	if (ft_strncmp(token_string, "-", 1) == 0)
+	{
+		if (ft_strncmp(node->token->content, "echo", 4) == 0
+			&& node->redir == NULL && node->where_redir == NULL)
+		{
+			while (token_string[i] == 'n')
+				i++;
+			if (token_string[i] == '\0' && node->params == NULL)
+				ft_lstadd_back_subnode(&node->params, ft_lstnew_subnode(token_string));
+		}
+		else
+			ft_lstadd_back_subnode(&node->params, ft_lstnew_subnode(token_string));
+		return (0);
+	}
+	return (1);
+}
 
 static	int	check_flags(t_node *tokens)
 {
@@ -18,6 +40,7 @@ static	int	check_flags(t_node *tokens)
 	t_subnode	*subnode;
 	int			flagecho;
 
+	detect_flag(tokens, tokens->token->content); //probably not the best implementation
 	flagecho = 0;
 	count = 0;
 	subnode = tokens->flags;
