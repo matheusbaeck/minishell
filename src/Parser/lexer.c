d1ca46ba82ba9764c676283a1807dcead6ee4efc
matheusbaeck <matheusbaeck@gmail.com>
2024-02-24 18:53:59 +0100

    pipparcer changed

--- src/Parser/lexer.c
+++ src/Parser/lexer.c
@@ -6,24 +6,98 @@
 /*   By: math <math@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 15:57:42 by smagniny          #+#    #+#             */
-/*   Updated: 2024/02/22 13:34:04 by math             ###   ########.fr       */
+/*   Updated: 2024/02/24 18:53:10 by math             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header.h"
 
+static int	is_pipe(int c)
+{
+	if (c == '|')
+		return (1);
+	return (0);
+}
+
+static int	is_quote(int c)
+{
+	if (c == '\'' || c == '\"')
+		return (1);
+	return (0);
+}
+
+// On success return index of closing char, if not find return '\0'
+static int	jumpe_content(const char	*line, char open, char close)
+{
+	int	nesting_level;
+	int	i;
+
+	i = 0;
+	nesting_level = 1;
+	while (line[++i])
+	{
+		if (close != open && line[i] == open)
+			nesting_level++;
+		else if (line[i] == close)
+		{
+			nesting_level--;
+			if (nesting_level == 0)
+				return (i);
+		}
+	}
+	return (i);
+}
+
+// On success return index of pipe, if not find return '\0'
+static int	pipechr(char *str)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	//printf("on pipechr:%s\n", str);
+	while (str[++i])
+	{
+		//printf("char:%c:", str[i]);
+		if (is_quote(str[i]))
+		{
+			j = i + jumpe_content(&str[i], str[i], str[i]);
+			//printf("quote %d to %d\n", i , j);
+			if (str[j] == '\0')
+				return (j);
+			i = j;
+		}
+		else if (is_pipe(str[i]))
+		{
+			//printf("pipe\n");
+			return (i);
+		}
+		//printf("none\n");
+	}
+	return (i);
+}
+
 static int	get_pipes(t_var *var, t_list **list)
 {
-	t_list	*temp;
+	char	*new_content;
+	int		i;
+	int		j;
 
-	*list = ft_lstnew(var->inputline);
-	if (pipe_split(list, is_cut_point) == -1)
-		return (ft_lstclear(list, free), -1);
-	temp = *list;
-	while (temp)
+	//printf("inicio: input:%s\n", var->inputline);
+	i = 0;
+	*list = NULL;
+	while (var->inputline[i])
 	{
-		temp = temp->next;
+		j = pipechr(&var->inputline[i]);
+		//printf("j:%d\n", j);
+		new_content = (char *)ft_calloc(j - i + 1, sizeof(char));
+		ft_strlcpy(new_content, &var->inputline[i], j - i + 1);
+		//printf("new_content:%s %d\n", new_content, ft_strlen(new_content));
+		ft_lstadd_back(list, ft_lstnew(new_content));
+		//printf("listlast:%s\n", (char *)ft_lstlast(*list)->content);
+		i += j;
 	}
+	free(var->inputline);
 	return (0);
 }
 
